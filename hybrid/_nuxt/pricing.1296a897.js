74aab45ff8c1b5d8a88b3b1c0063abae0ade4881
30nama Automation <automation@30nama.com>
2023-10-21 11:41:50 +0000

    Deployed via 30nama Automation System

---
+++ hybrid/_nuxt/pricing.1296a897.js
@@ -0,0 +1,1 @@
+import{aK as a}from"./entry.b873eff6.js";const o=a("pricing",{state:()=>({plans:[],gateway:"shaparak",discountCode:void 0})});export{o as u};
---
+++ hybrid/_nuxt/useBackground.d678504f.js
@@ -0,0 +1,1 @@
+import{a3 as o}from"./entry.b873eff6.js";function r(a){const e=o("backgroundImageUrl");e.value=a}export{r as u};
