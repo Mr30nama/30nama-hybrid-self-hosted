1151ad0e85ad13df04b3f6ae9e6a954617b0c143
30nama Automation <automation@30nama.com>
2023-10-21 09:08:55 +0000

    Deployed via 30nama Automation System

---
+++ hybrid/_nuxt/index.e122233b.js
@@ -0,0 +1,1 @@
+import{d as e,l as r,g as o,h as t}from"./entry.8b93e9fc.js";const u=e({__name:"index",setup(s){return r().replace("/discover/categories"),(c,n)=>(o(),t("div"))}});export{u as default};
---
+++ hybrid/_nuxt/useBackground.c11a2457.js
@@ -0,0 +1,1 @@
+import{a3 as o}from"./entry.8b93e9fc.js";function r(a){const e=o("backgroundImageUrl");e.value=a}export{r as u};
